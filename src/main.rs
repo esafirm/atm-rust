3e7c497423bffbaffb9c0369495e61480d0c0ab9
Esa Firman <esafirm21@gmail.com>
2024-11-19 06:06:06 +0700

    iniital implementation

---
+++ src/main.rs
@@ -0,0 +1,148 @@
+use clap::{Parser, Subcommand};
+use std::{
+    collections::HashMap,
+    io::{self, Write},
+};
+
+#[derive(Parser, Debug)]
+#[command(author, about)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    Login { name: String },
+    Logout,
+    Deposit { amount: f64 },
+    Withdraw { amount: f64 },
+    Transfer { amount: f64, to: String },
+}
+
+struct Balance {
+    amount: f64,
+}
+
+impl Default for Balance {
+    fn default() -> Self {
+        Self { amount: 0.0 }
+    }
+}
+
+// Let's have a state machine to handle the commands
+struct StateMachine {
+    logged_in_user: Option<String>,
+}
+
+fn get_or_create_balance<'a>(
+    balances: &'a mut HashMap<String, Balance>,
+    user: &str,
+) -> &'a mut Balance {
+    balances
+        .entry(user.to_string())
+        .or_insert_with(Balance::default)
+}
+
+fn main() {
+    println!("Welcome to the Bank Bank Toot ATM! Type 'exit' to quit.");
+
+    let mut state = StateMachine {
+        logged_in_user: None,
+    };
+
+    let mut balances: HashMap<String, Balance> = HashMap::new();
+
+    loop {
+        print!("> ");
+        io::stdout().flush().unwrap();
+
+        let mut input = String::new();
+        io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+
+        if input == "exit" {
+            break;
+        }
+
+        // Implement ctrl + D shortcut
+        if input.is_empty() {
+            break;
+        }
+
+        // Add a dummy program name to the start of the args
+        let mut args = vec!["program".to_string()];
+        args.extend(input.split_whitespace().map(String::from));
+        let cli = Cli::try_parse_from(args);
+
+        let is_logged_in = state.logged_in_user.is_some();
+        if !is_logged_in {
+            // Check command before login
+            match &cli {
+                Ok(cli) => match &cli.command {
+                    Some(Commands::Login { name }) => {
+                        state.logged_in_user = Some(name.clone());
+                        println!("You are already logged in as {}", name);
+                    }
+                    None | Some(_) => {
+                        println!("You are not logged in. Please `login` to continue");
+                    }
+                },
+                Err(_e) => println!("Command not recognized"),
+            }
+            continue;
+        }
+
+        let user = state.logged_in_user.clone().unwrap();
+
+        // Check command after login
+        match &cli {
+            Ok(cli) => match &cli.command {
+                Some(Commands::Logout) => {
+                    state.logged_in_user = None;
+                    println!("Logging out");
+                    break;
+                }
+                Some(Commands::Deposit { amount }) => {
+                    let balance = get_or_create_balance(&mut balances, &user);
+                    balance.amount += amount;
+
+                    println!("Deposited ${}", amount);
+                    println!("New balance: ${}", balance.amount);
+                }
+
+                Some(Commands::Withdraw { amount }) => {
+                    let balance = get_or_create_balance(&mut balances, &user);
+                    if balance.amount < *amount {
+                        println!("Insufficient funds. Your balance is ${}", balance.amount);
+                        continue;
+                    }
+                    balance.amount -= amount;
+                    println!("Withdrew ${}", amount);
+                    println!("New balance: ${}", balance.amount);
+                }
+
+                Some(Commands::Transfer { amount, to }) => {
+                    let balance = get_or_create_balance(&mut balances, &user);
+                    if balance.amount < *amount {
+                        println!("Insufficient funds. Your balance is ${}", balance.amount);
+                        continue;
+                    }
+                    balance.amount -= amount;
+                    let new_balance = balance.amount;
+
+                    let to_balance = get_or_create_balance(&mut balances, to);
+                    to_balance.amount += amount;
+
+                    println!("Transferred ${} to {}", amount, to);
+                    println!("New balance: ${}", new_balance);
+                }
+                Some(Commands::Login { name }) => println!("You are already logged in as {}", name),
+                None => println!("No command specified"),
+            },
+            Err(e) => println!("Error: {}", e),
+        }
+    }
+
+    println!("Goodbye!");
+}
